7b6fd93b24f6e82e08247a839dc06f22ade3db64
ryan spangler <ryan.spangler@gmail.com>
2012-05-09 12:01:31 -0700

    removed some things

--- site/src/skel/controllers/home.clj
+++ site/src/skel/controllers/home.clj
@@ -1,14 +1,6 @@
 (ns skel.controllers.home
-  (:use caribou.debug
-        [cheshire.core :only (generate-string)])
-  (:require [clj-time.core :as time-core]
-            [clj-time.format :as format]
-            [clj-time.coerce :as coerce]
-            [caribou.db :as db]
-            [caribou.model :as model]
-            [caribou.config :as config]))
+  (:use caribou.app.controller))
 
 (defn home
   [params]
-  (let [template (params :template)]
-    (template params)))
+  (render params))
