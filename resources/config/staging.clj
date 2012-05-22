8cd76357d3e20e35ade6ab2008649933bf1bcfb7
eric feigner <eric@weareinstrument.com>
2012-05-22 16:10:27 -0700

    simplifying staging / production configs

--- resources/config/staging.clj
+++ resources/config/staging.clj
@@ -4,10 +4,4 @@
             :database     "caribou_staging"
             :user         "postgres"
             :password     "postgres"}
- :template-dir   "site/resources/templates" 
- :controller-ns  "skel.controllers"
- :public-dir     "site/resources/public"
- :api-public     "api/public"
- :asset-dir      "assets"
- :halo-key    "replace-with-halo-key"
- :halo-host   "http://localhost:33333"}
+ :asset-dir      "assets"}
