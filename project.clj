3be9a4c3b458f462b47f2ba7fa59ee2f8731af1f
Justin Glenn Smith <noisesmith@gmail.com>
2012-07-12 15:01:20 -0700

    needed dep to configure logging

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   "caribou: test site for development purposes"
 
   :dependencies
-   [[org.clojure/clojure "1.3.0"]
-    [antler/lein-caribou "1.4.5"]]
+  [[clj-logging-config "1.9.8"]
+   [org.clojure/clojure "1.3.0"]
+   [antler/lein-caribou "1.4.5"]]
 
   :sub
    ["admin"
