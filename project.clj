87803a079e1e6e6a688f6bc90ee396b5e51a4838
Justin Glenn Smith <noisesmith@gmail.com>
2012-07-17 13:06:19 -0700

    removed unneeded dependency

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   "caribou: test site for development purposes"
 
   :dependencies
-  [[clj-logging-config "1.9.8"]
-   [org.clojure/clojure "1.3.0"]
+  [[org.clojure/clojure "1.3.0"]
    [antler/lein-caribou "1.4.5"]]
 
   :sub
