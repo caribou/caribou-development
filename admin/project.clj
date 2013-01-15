096d6d8ce22088545ebc7c148a34904c5af2e1d8
Justin Glenn Smith <noisesmith@gmail.com>
2013-01-15 13:16:55 -0800

    lining up versions to make a new bundle for lein caribou create

--- admin/project.clj
+++ admin/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [antler/caribou-admin "0.4.7"]]
+                 [antler/caribou-admin "0.7.6"]]
   :resource-paths ["resources/" "../resources/"]            
   :ring {:handler caribou.admin.core/app
          :servlet-name "caribou-development-admin"
