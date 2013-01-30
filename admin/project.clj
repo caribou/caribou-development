b773f9926ec942a2ec6954b0e0332dd0accbbb49
Justin Glenn Smith <noisesmith@gmail.com>
2013-01-29 17:00:19 -0800

    fixed namespaces so hooks can be found

--- admin/project.clj
+++ admin/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [antler/caribou-admin "0.7.6"]]
+  :source-paths ["src" "../src"]
   :resource-paths ["resources/" "../resources/"]            
   :ring {:handler caribou.admin.core/app
          :servlet-name "caribou-development-admin"
