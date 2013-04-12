0c186d93460a8ec189d62bb68b8609f039361c4e
ryan spangler <ryan.spangler@gmail.com>
2013-04-11 17:25:02 -0700

    flattening

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject caribou-devsite "0.1.0"
+(defproject caribou-devsite "0.10.0"
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [antler/caribou-frontend "0.9.23"]
-                 [antler/caribou-admin "0.9.15"]
-                 [antler/caribou-api "0.9.7"]
+                 [antler/caribou-frontend "0.10.0"]
+                 [antler/caribou-admin "0.10.0"]
+                 [antler/caribou-api "0.10.0"]
                  [swank-clojure "1.4.2"]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   :source-paths ["src" "../src"]
