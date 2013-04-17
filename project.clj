543ea0d603502ebd023a9fd4ff06ea2314ccb772
Kyle Dawkins <kyle.dawkins@weareinstrument.com>
2013-04-16 17:05:44 -0700

    Minor fixes, version bump

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [antler/caribou-frontend "0.10.0"]
-                 [antler/caribou-admin "0.10.0"]
+                 [antler/caribou-admin "0.10.2"]
                  [antler/caribou-api "0.10.0"]
                  [swank-clojure "1.4.2"]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   :source-paths ["src" "../src"]
-  :resource-paths ["resources/" "../resources/"]            
+  :resource-paths ["resources/" "../resources/"]
   :migration-namespace skel.migrations
   :immutant {:context-path "/"}
   :ring {:handler skel.core/handler
--- src/skel/core.clj
+++ src/skel/core.clj
@@ -90,4 +90,3 @@
         (wrap-content-type)
         (wrap-session)
         (wrap-cookies))))
-
