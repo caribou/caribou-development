dc076bc6f96263f9a5ee4674a11137a03c57b32d
ryan spangler <ryan.spangler@gmail.com>
2013-06-21 14:29:06 -0700

    updated to latest libraries

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject caribou-devsite "0.11.1"
+(defproject caribou-devsite "0.11.0"
   :description "The page routing ring handler for caribou"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.1.8"]
                  [org.immutant/immutant "0.10.0"]
-                 [antler/caribou-frontend "0.11.1"]
-                 [antler/caribou-admin "0.11.1"]
-                 [antler/caribou-api "0.11.1"]
+                 [antler/caribou-frontend "0.11.24"]
+                 [antler/caribou-admin "0.11.30"]
+                 [antler/caribou-api "0.11.20"]
                  [swank-clojure "1.4.2"]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   :source-paths ["src" "../src"]
