b98061a145297b32f91900e9a648c340a5ffaf23
Justin Smith <noisesmith@gmail.com>
2014-03-26 15:52:05 -0700

    lein-caribou and clojure versions

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject caribou-development "0.14.0"
+(defproject caribou-development "0.14.0-PLUGINS"
   :description "The page routing ring handler for caribou"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.12"]
                  [caribou/caribou-admin "0.14.0"]
                  [caribou/caribou-api "0.14.0"]
                  [schmetterling "0.0.8"]
                  [org.clojure/tools.nrepl "0.2.3"]]
   :plugins [[lein-ring "0.8.6"]
-            [caribou/lein-caribou "2.13.4"]
+            [caribou/lein-caribou "2.14.0"]
             [lein-cljsbuild "1.0.2"
              :exclusions [fs]]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n" 
